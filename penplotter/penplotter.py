7e93f8c1222624c28f7cbc93e39fa1b35bb0ba6a
YewZiHon <82492224+YewZiHon@users.noreply.github.com>
2023-12-27 22:22:39 +0800

    Update penplotter.py

--- penplotter/penplotter.py
+++ penplotter/penplotter.py
@@ -1,10 +1,25 @@
 import cv2 as cv
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+import threading
+
 
 while True:
+    #setup tkinter
+    root = Tk()
+    width= root.winfo_screenwidth()               
+    height= root.winfo_screenheight()               
+    root.geometry("%dx%d" % (width, height))
+    root.title("Plotter")
+    frm = ttk.Frame(root, padding=10)
+    frm.pack(side=CENTER)
+    ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+    root.mainloop()
+    print("done")
     #get an image
+
         #check if image is fine
-    
+
     #convert to g code
-    #start plotting
-    pass
+    #start plotting 
