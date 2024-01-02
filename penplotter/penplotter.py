97ed50294d3770b92a5bb9f487cf2520002cb334
YewZiHon <82492224+YewZiHon@users.noreply.github.com>
2024-01-02 22:38:01 +0800

    Update penplotter.py

--- penplotter/penplotter.py
+++ penplotter/penplotter.py
@@ -12,7 +12,9 @@ import enum
 import potrace
 from svg_to_gcode.svg_parser import parse_file
 from svg_to_gcode.compiler import Compiler, interfaces
-
+import time
+import serial
+import glob
 
 
 state = enum.Enum('state', ['takePhoto', 'previewPhoto', 'gen', 'plot', 'paused'])
@@ -120,6 +122,10 @@ class plotter:
         self.state=state.gen
         self.root.bind('<Escape>', None)
         self.root.bind('<space>', None)
+        self.btn1.pack_forget()
+        self.btn2.pack_forget()
+        self.btn1.update_idletasks()
+        self.btn2.update_idletasks()
 
         print("Gcode")
 
@@ -165,14 +171,6 @@ class plotter:
             fp.write("</svg>")
 
         
-        preview = Image.fromarray(cv2.bitwise_not(cv2.imread(sys.path[0]+"\\temp\\img\\"+self.filename+"canny.jpeg")))
-        preview = ImageTk.PhotoImage(preview)
-        
-        self.panel.configure(image=preview)
-        self.panel.image = preview
-        self.panel.update_idletasks()
-
-        
         class CustomInterface(interfaces.Gcode):
             def __init__(self):
                 super().__init__()
@@ -191,10 +189,104 @@ class plotter:
 
         gcode_compiler.append_curves(curves) 
         gcode_compiler.compile_to_file(sys.path[0]+"\\temp\\gcode\\"+self.filename+".gcode", passes=1)
+
+        with open(sys.path[0]+"\\temp\\gcode\\"+self.filename+".gcode", 'r+') as f:
+            content = f.read()
+            f.seek(0, 0)
+            f.write("G28\n" + '\n' + content)
         
+
+        preview = Image.fromarray(cv2.bitwise_not(cv2.imread(sys.path[0]+"\\temp\\img\\"+self.filename+"canny.jpeg")))
+        preview = ImageTk.PhotoImage(preview)
+        
+        self.panel.configure(image=preview)
+        self.panel.image = preview
+        self.panel.update_idletasks()
 
 
         print("Gen done")
+        self.state=state.plot
+        self.thread = threading.Thread(target=self.sendGcode, args=(), daemon=True)
+        self.thread.start()
+        self.btn1 = tkinter.Button(self.root, text="Pause",command=self.pause)
+        self.btn1.pack(anchor=tkinter.SW, padx=10,pady=10)
+        self.btn2 = tkinter.Button(self.root, text="Stop",command=self.stop)
+        self.btn2.pack(anchor=tkinter.SE, padx=10,pady=10)
+        #self.thread.start()
+        #self.sendGcode()
+        
+    def pause(self):
+        if self.state==state.plot:
+            self.state=state.paused
+        elif self.state==state.paused:
+            self.state=state.plot
+        
+    def stop(self):
+        if self.state==state.plot or self.state==state.paused:
+            self.state=state.takePhoto
+
+    def serial_ports(self):
+    
+        if sys.platform.startswith('win'):
+            ports = ['COM%s' % (i + 1) for i in range(256)]
+        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+            # this excludes your current terminal "/dev/tty"
+            ports = glob.glob('/dev/tty[A-Za-z]*')
+        elif sys.platform.startswith('darwin'):
+            ports = glob.glob('/dev/tty.*')
+        else:
+            raise EnvironmentError('Unsupported platform')
+
+        result = []
+        for port in ports:
+            try:
+                s = serial.Serial(port)
+                s.close()
+                result.append(port)
+            except (OSError, serial.SerialException):
+                pass
+        return result   
+
+    def sendGcode(self):
+        #open serial
+        ports = self.serial_ports()
+        if "COM3" in ports:
+            ports.pop(ports.index("com3"))
+
+        if ports == []:
+            print("Error opening serial")
+            self.state=state.takePhoto
+            self.startStream()
+            return
+        print("ports",ports)
+        s = serial.Serial(ports[0],115200)
+
+        f = open(sys.path[0]+"\\temp\\gcode\\"+self.filename+".gcode",'r')
+
+        s.write(b"\r\n\r\n") # Hit enter a few times to wake the Printrbot
+        time.sleep(2)   # Wait for Printrbot to initialize
+        s.flushInput()  # Flush startup text in serial input
+
+        while True:
+            if self.state==state.plot:
+                #send g code
+                l=f.readline()
+                l = l.strip() # Strip all EOL characters for streaming
+                if  (l.isspace()==False and len(l)>0) :
+                    print ('Sending:',  l)
+                    s.write(bytearray(l + '\n','utf-8')) # Send g-code block
+                    grbl_out = s.readline() # Wait for response with carriage return
+                    print (grbl_out.strip())
+                
+            elif self.state==state.paused:
+                time.sleep(0.1)
+                pass
+            elif self.state==state.takePhoto or self.state==state.previewPhoto or self.state==state.gen:
+                break
+
+        self.state=state.takePhoto
+        self.startStream()
+        return
 
     def on_closing(self):
         self.root.destroy()
